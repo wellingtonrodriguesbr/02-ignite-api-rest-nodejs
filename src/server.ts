7a12c37071979366d076f26eae7d72d5ff105b29
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-02-25 10:04:06 -0300

    feat: add knex and setup

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
+
+app.get('/', async (req, res) => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
 
 app
   .listen({
