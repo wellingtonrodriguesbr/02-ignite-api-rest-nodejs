cefd7911ef052b7e8b3afbac88100bc43f87e15a
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-03-07 05:26:50 -0300

    test: list all transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,21 +1,47 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
-import { app } from '../src/app'
-import request from 'supertest'
+import { it, beforeAll, afterAll, describe, expect } from "vitest";
+import { app } from "../src/app";
+import request from "supertest";
 
-describe('Transactions routes', () => {
+describe("Transactions routes", () => {
   beforeAll(async () => {
-    await app.ready()
-  })
-  
+    await app.ready();
+  });
+
   afterAll(async () => {
-    await app.close()
-  })
-  
-  it('should be able to create a new transaction', async () => {
-   const response = await request(app.server).post('/transactions').send({
-      title: 'Example title',
-      amount: 5000,
-      type: 'credit'
-    }).expect(201)
-  })
-})
+    await app.close();
+  });
+
+  it("should be able to create a new transaction", async () => {
+    await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "Example title",
+        amount: 5000,
+        type: "credit",
+      })
+      .expect(201);
+  });
+
+  it("should be able to list all transaction", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "Example title",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "Example title",
+        amount: 5000,
+      }),
+    ]);
+  });
+});
